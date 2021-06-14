1b91c2973bcc9725f53457d7530d2054cbdba136
gaborsteber <stebergabor@gmail.com>
2021-06-14 18:41:50 +0200

    ujbóli feltöltés

--- src/com/company/Subscriber.java
+++ src/com/company/Subscriber.java
@@ -1,7 +1,14 @@
 package com.company;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class Subscriber {
     private final String subscriber;
@@ -63,5 +70,61 @@ public class Subscriber {
 
     public void setTemperatures(List<Temperature> temperatures) {
         this.temperatures = temperatures;
+    }
+
+    public int getBoilerTypeInt()
+    {
+        int boilerType = 0;
+        if(this.getBoilerType().equals("Boiler 1200W"))
+        {
+            boilerType = 1;
+        }
+        else if (this.getBoilerType().equals("Boiler p5600"))
+        {
+            boilerType = 2;
+        }
+        return  boilerType;
+    }
+    public int getAirTypeInt()
+    {
+        int airType = 0;
+        if(this.getAirConditionerType().equals("Air p5600"))
+        {
+            airType = 1;
+        }
+        else if (this.getAirConditionerType().equals("Air c320"))
+        {
+            airType = 2;
+        }
+        return  airType;
+    }
+    public static double getTemperatureForNow(Subscriber subscriber) throws ParseException {
+        int temperatureNow = 0;
+        SimpleDateFormat formatter = new SimpleDateFormat("HH");
+        Date date = new Date();
+        LocalTime time = LocalTime.now();
+        int index = 0;
+        for(int i=0; i<subscriber.getTemperatures().size(); i++)
+        {
+            String startOfPeriod = subscriber.getTemperatures().get(i).getPeriod().substring(0,2) + ":00";
+            String endOfPeriod;
+            int endOfHour = parseInt(subscriber.getTemperatures().get(i).getPeriod().substring(3,5))-1;
+            String hourBackToString = Integer.toString(endOfHour);
+            if(hourBackToString.length() == 1)
+            {
+                endOfPeriod = "0" + hourBackToString +":59";
+            }
+            else endOfPeriod = hourBackToString + ":59";
+
+            LocalTime startP = LocalTime.parse(startOfPeriod);
+            LocalTime endP = LocalTime.parse(endOfPeriod);
+            //System.out.println(startP + " - " + endP);
+            if(time.isAfter(startP) && time.isBefore(endP))
+            {
+                index = i;
+            }
+        }
+        //System.out.println(subscriber.getTemperatures().get(index).getTemperature());
+        return subscriber.getTemperatures().get(index).getTemperature();
     }
 }
